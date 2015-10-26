443454167f33e941c23e2e41e0d092d84f237e61
Simon Nielsen <simon.nielsen@svt.se>
2015-10-26 17:36:34 +0100

    Reset repo

--- lib/grunt.sh
+++ lib/grunt.sh
@@ -3,11 +3,7 @@ run_grunt() {
     echo "-----> Found Gruntfile, running grunt heroku task"
     if [ -f $build_dir/node_modules/grunt-cli/bin/grunt ]; then
       $build_dir/node_modules/grunt-cli/bin/grunt heroku
-    elif [ -f $build_dir/node_modules/grunt/bin/grunt ]; then
-      $build_dir/node_modules/grunt/bin/grunt heroku
     else
-      echo "       Could not find local grunt, force install..."
-      npm install grunt
       $build_dir/node_modules/grunt/bin/grunt heroku
     fi
   else
